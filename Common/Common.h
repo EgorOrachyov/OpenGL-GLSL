35801914f14c91b0e259e4c2bc99240d8c3f7a60
egororachyov <egororachyov5@gmail.com>
2018-04-10 22:22:27 +0300

    Added glfw context creation and common debug methods

---
+++ Common/Common.h
@@ -0,0 +1,8 @@
+//
+// Created by Egor Orachyov on 10.04.2018.
+//
+
+#ifndef OPENGL_GLSL_COMMON_H
+#define OPENGL_GLSL_COMMON_H
+
+#endif //OPENGL_GLSL_COMMON_H
---
+++ Common/Context.h
@@ -0,0 +1,8 @@
+//
+// Created by Egor Orachyov on 10.04.2018.
+//
+
+#ifndef OPENGL_GLSL_CONTEXT_H
+#define OPENGL_GLSL_CONTEXT_H
+
+#endif //OPENGL_GLSL_CONTEXT_H
